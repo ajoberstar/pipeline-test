2e83f43f0ee04faab5f340034ac45140df00383c
Andrew Oberstar <ajoberstar@gmail.com>
2018-04-05 21:08:50 -0500

    Try encrypted file

---
+++ src/main/java/org/ajoberstar/pipeline/test/PipelineTestPlugin.java
@@ -0,0 +1,10 @@
+package org.ajoberstar.pipeline.test;
+
+import org.gradle.api.Project;
+import org.gradle.api.Plugin;
+
+public class PipelineTestPlugin implements Plugin<Project> {
+  public void apply(Project project) {
+    
+  }
+}
