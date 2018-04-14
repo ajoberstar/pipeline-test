8c706a1a937a0a4af14d50486be8be580d87f7dd
Andrew Oberstar <ajoberstar@gmail.com>
2018-04-14 12:58:10 -0500

    Generated by gradle-git-publish

---
+++ main/java/org/ajoberstar/pipeline/test/PipelineTestPlugin.java
@@ -0,0 +1,8 @@
+package org.ajoberstar.pipeline.test;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class PipelineTestPlugin implements Plugin<Project> {
+  public void apply(Project project) {}
+}
---
+++ test/java/com/example/MyTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.junit.Test;
+
+public class MyTest {
+  @Test
+  public void testStuff() {
+    // throw new AssertionError("Stuff");
+  }
+}
